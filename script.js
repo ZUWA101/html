1d8a1677614831ae272cdc0ae439ab63c2f0a1e7
Zuwa101 <odighizuwaosasuyi@gmail.com>
2026-09-08 18:11:41 +0100

    Separated CSS and JS into separate files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//this is a js file
+        //this is a scirpt file
+        function validateLogin(){
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            if(username === 'admin' && password === 'password123'){
+                alert('Login successful!');
+            } else {
+                alert('Invalid username or password.');
+            }
+        }
+
